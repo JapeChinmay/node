9715dceeb11f67110214e052cc1546e383f63052
Chinmay Jape <iam95041@gmail.com>
2023-01-18 15:47:04 +0530

    added REST , post ,get using postman here

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,16 +1,46 @@
 const express = require('express');
 
+const fs = require('fs');
+
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello form the server side', name: 'chinmay jape' });
-});
-app.post('/', (req, res) => {
-  res.status(200).json({ message: 'this is post one' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'succes',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
+/////
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'succes',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
 const port = 3000;
 
 app.listen(port, () => {
