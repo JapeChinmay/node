d9b92d6a6f6ded2e01f6e0bd8917f9de163f219d
Chinmay Jape <iam95041@gmail.com>
2023-01-18 16:24:26 +0530

    addedd CRUD

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -9,12 +9,27 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+app.get('/api/v1/tours/:id?', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!id) {
+    return res.status(200).json({
+      status: 'succes',
+      data: {
+        tours: tours,
+      },
+    });
+  }
+
+  if (!tour) {
+    return res.status(404).json({ message: 'failed' });
+  }
   res.status(200).json({
     status: 'succes',
     results: tours.length,
     data: {
-      tours: tours,
+      tours: tour,
     },
   });
 });
@@ -40,6 +55,32 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'invalid' });
+  }
+
+  res.status(200).json({
+    status: 'succes',
+    data: {
+      tour: 'updated',
+    },
+  });
+
+  res.send('done patch');
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'invalid id' });
+  }
+
+  res.status(204).json({
+    status: 'succes',
+    data: 'null',
+  });
 });
 const port = 3000;
 
